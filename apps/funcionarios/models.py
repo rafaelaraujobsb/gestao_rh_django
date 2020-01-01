411a3816b5789e6b080c5d1b67de079bb6429c57
Rafael Araujo <bsb.rafaelaraujo@gmail.com>
2020-01-01 16:14:46 -0300

    criação das ligações das tabelas

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from apps.empresas.models import Empresa
+from apps.departamentos.models import Departamento
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text="Nome do funcionário")
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
