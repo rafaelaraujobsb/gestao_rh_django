087fd5ac782ac99799bef0569b43a16925368acb
Rafael Araujo <bsb.rafaelaraujo@gmail.com>
2020-01-01 17:55:10 -0300

    criação do login e logout

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def home(request):
-    return HttpResponse('Olá')
+    return render(request, 'core/index.html')
